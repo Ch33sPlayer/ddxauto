f0428d9d50796bf9fa082aaaf960a294c16f5fcb
Владислав Максимов <v.maksimov@ddxfitness.local>
2023-07-13 10:43:08 +0300

    Запрос на получение списка клубов, что он отдает 200 код ответа

---
+++ tests/getClubs.test.ts
@@ -0,0 +1,28 @@
+import { expect, request, test } from "@playwright/test";
+import api from '../api.json';
+
+test.describe ("Api - тест на получение списка клубов " , async () => { 
+    test("[positive] получить список клубов",async ({ request }) => {
+        const response = await request.get(
+            `${api.urls.base_url_api}${api.paths.clubs}`,
+            {
+                headers : {
+                    'Authorization': `${api.tokens.test}`
+                },
+                params :{
+                    session_id:"1",
+                    request_id:"2",
+                    request_source:"crm"
+                }
+            }
+        );
+        
+        expect(response.status()).toEqual(200);
+    });
+
+
+test("[negative] получить список клубов",async ({ request }) => {
+// Здесь будет мой прекарсный код, который как раз и проверит
+
+});
+});
